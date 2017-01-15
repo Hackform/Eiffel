a1c457848b5277b7b4ad5800d9101180c8320ebd
Kevin Wang <wangkevin448@gmail.com>
2017-01-14 17:33:52 -0800

    q testing

--- service/repo/q/q_test.go
+++ service/repo/q/q_test.go
@@ -5,12 +5,40 @@ import (
 	"testing"
 )
 
-func Test_Q_New(t *testing.T) {
+func Test_Q_NewQOne(t *testing.T) {
 	assert := assert.New(t)
 	query := NewQOne("test_sector", Props{"prop_1", "prop_2", "another_prop"}, Constraints{NewCon("key_1", EQUAL, "value_1"), NewCon("key_2", EQUAL, "value_2")})
 	assert.Equal(ACTION_QUERY_ONE, query.Action, "property Action should be instantiated")
 	assert.Equal("test_sector", query.Sector, "property Sector should be instantiated")
 	assert.Equal(3, len(query.RProps), "property RProps should be instantiated")
+	assert.Equal(2, len(query.Cons), "property Cons should be instantiated")
+}
+
+func Test_Q_NewQMulti(t *testing.T) {
+	assert := assert.New(t)
+	query := NewQMulti("test_sector", Props{"prop_1", "prop_2", "another_prop"}, Constraints{NewCon("key_1", EQUAL, "value_1"), NewCon("key_2", EQUAL, "value_2")}, 5)
+	assert.Equal(ACTION_QUERY_MULTI, query.Action, "property Action should be instantiated")
+	assert.Equal("test_sector", query.Sector, "property Sector should be instantiated")
+	assert.Equal(3, len(query.RProps), "property RProps should be instantiated")
+	assert.Equal(2, len(query.Cons), "property Cons should be instantiated")
+	assert.Equal(5, query.Limit, "property Limit should be instantiated")
+}
+
+func Test_Q_NewI(t *testing.T) {
+	assert := assert.New(t)
+	query := NewI("test_sector", Props{"prop_1", "prop_2", "another_prop"}, Props{"val_1", "val_2", "val_3"})
+	assert.Equal(ACTION_INSERT, query.Action, "property Action should be instantiated")
+	assert.Equal("test_sector", query.Sector, "property Sector should be instantiated")
+	assert.Equal(3, len(query.RProps), "property RProps should be instantiated")
+	assert.Equal(len(query.RProps), len(query.Vals), "property Vals should be instantiated")
+}
+
+func Test_Q_NewU(t *testing.T) {
+	assert := assert.New(t)
+	query := NewU("test_sector", Constraints{NewCon("key_1", EQUAL, "value_1"), NewCon("key_2", EQUAL, "value_2")}, Constraints{NewCon("key_1", EQUAL, "value_1"), NewCon("key_2", EQUAL, "value_2")})
+	assert.Equal(ACTION_UPDATE, query.Action, "property Action should be instantiated")
+	assert.Equal("test_sector", query.Sector, "property Sector should be instantiated")
+	assert.Equal(2, len(query.Mods), "property RProps should be instantiated")
 	assert.Equal(2, len(query.Cons), "property Cons should be instantiated")
 }
 
