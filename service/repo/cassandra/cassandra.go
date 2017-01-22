b547ae07e2aaa1cb303e4314cf320bcef2852071
Kevin Wang <wangkevin448@gmail.com>
2017-01-21 21:18:40 -0800

    setup table

--- service/repo/cassandra/cassandra.go
+++ service/repo/cassandra/cassandra.go
@@ -5,6 +5,53 @@ import (
 	"github.com/gocassa/gocassa"
 )
 
+//////////
+// Opts //
+//////////
+
+type (
+	cassOpts struct {
+		model      interface{}
+		kpartition []string
+		kcluster   []string
+	}
+
+	Config map[string]*cassOpts
+)
+
+func Opts(model interface{}, kpartition, kcluster []string) *cassOpts {
+	return &cassOpts{
+		model:      model,
+		kpartition: kpartition,
+		kcluster:   kcluster,
+	}
+}
+
+///////////
+// Setup //
+///////////
+
+type (
+	setupModel struct {
+		name    string `cql:"eiffel_name"`
+		setup   bool   `cql:"eiffel_setup_complete"`
+		version string `cql:"eiffel_version"`
+	}
+)
+
+const (
+	setup_table_name = "eiffel_setup"
+	setup_table_pk   = "eiffel_name"
+)
+
+func sampleSetupModel() *setupModel {
+	return &setupModel{}
+}
+
+///////////////
+// Cassandra //
+///////////////
+
 type (
 	Cassandra struct {
 		keySpace gocassa.KeySpace
@@ -19,31 +66,7 @@ type (
 		username,
 		password string
 	}
-
-	cassOpts struct {
-		model      interface{}
-		kpartition []string
-		kcluster   []string
-	}
-
-	Config map[string]*cassOpts
 )
-
-//////////
-// Opts //
-//////////
-
-func Opts(model interface{}, kpartition, kcluster []string) *cassOpts {
-	return &cassOpts{
-		model:      model,
-		kpartition: kpartition,
-		kcluster:   kcluster,
-	}
-}
-
-///////////////
-// Cassandra //
-///////////////
 
 func New(keyspace string, nodeIps []string, username, password string, config Config) *Cassandra {
 	return &Cassandra{
@@ -64,6 +87,8 @@ func (c *Cassandra) Start() bool {
 	}
 	c.keySpace = keyspace
 
+	c.config[setup_table_name] = Opts(sampleSetupModel(), []string{setup_table_pk}, nil)
+
 	for k, v := range c.config {
 		c.space[k] = c.keySpace.Table(k, v.model, gocassa.Keys{
 			PartitionKeys:     v.kpartition,
@@ -78,6 +103,10 @@ func (c *Cassandra) Shutdown() {
 
 func (c *Cassandra) Transaction() (repo.Tx, error) {
 	return newTx(c)
+}
+
+func (c *Cassandra) Setup() error {
+	return nil
 }
 
 /////////////////
--- service/repo/repo.go
+++ service/repo/repo.go
@@ -7,6 +7,7 @@ type (
 		Start() bool
 		Shutdown()
 		Transaction() (Tx, error)
+		Setup() error
 	}
 
 	Tx interface {
