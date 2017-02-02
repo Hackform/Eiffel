60d28b26342f2c96a2b4e3e0be692d5274dfe665
Kevin Wang <wangkevin448@gmail.com>
2017-02-01 21:39:39 -0800

    users route

--- route/users/users.go
+++ route/users/users.go
@@ -1,7 +1,6 @@
 package userroute
 
 import (
-	"context"
 	"fmt"
 	"github.com/Hackform/Eiffel"
 	"github.com/Hackform/Eiffel/model/user"
@@ -39,63 +38,47 @@ func New(repository repo.Repo) eiffel.Route {
 }
 
 var (
-	regexUsername = regexp.MustCompile(`[a-zA-Z0-9_]{1,32}`)
+	regexUsername = regexp.MustCompile(`^[a-zA-Z0-9_\-]{1,32}$`)
+	regexUserid   = regexp.MustCompile(`^[a-zA-Z0-9_\-=]{1,32}$`)
 )
 
 func isValidUsername(username string) bool {
 	return regexUsername.MatchString(username)
 }
-
-type (
-	contextKey string
-)
-
-const (
-	ctxKeyUsermodel = contextKey("usermodel")
-)
+func isValidUserid(userid string) bool {
+	return regexUserid.MatchString(userid)
+}
 
 func (rr *userroute) Register(r chi.Router) {
 	r.Route("/u/:username", func(r chi.Router) {
-		r.Use(func(next http.Handler) http.Handler {
-			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				username := chi.URLParam(r, "username")
-
-				if !isValidUsername(username) {
-					render.Status(r, http.StatusBadRequest)
-					render.JSON(w, r, resUsersErr{
-						Message: "Username must be between 1 and 32 characters",
-					})
-					return
-				}
-
-				tx, err := rr.repo.Transaction()
-				if err != nil {
-					lg.RequestLog(r).WithError(err).Error("Failed to acquire transaction")
-					render.Status(r, http.StatusInternalServerError)
-					render.JSON(w, r, resUsersErr{
-						Message: "Failed transaction",
-					})
-					return
-				}
-
-				usermodel, err := user.SelectByUsername(tx, username)
-				if err != nil {
-					lg.RequestLog(r).WithError(err).Warn("User not found")
-					render.Status(r, http.StatusNotFound)
-					render.JSON(w, r, resUsersErr{
-						Message: "User not found",
-					})
-					return
-				}
-
-				ctx := context.WithValue(r.Context(), ctxKeyUsermodel, usermodel)
-				next.ServeHTTP(w, r.WithContext(ctx))
-			})
-		})
-
 		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			username := chi.URLParam(r, "username")
+			if !isValidUsername(username) {
+				render.Status(r, http.StatusBadRequest)
+				render.JSON(w, r, resUsersErr{
+					Message: "Username must be between 1 and 32 characters and only be composed of a-z, A-Z, 0-9, _, -",
+				})
+				return
+			}
+			tx, err := rr.repo.Transaction()
+			if err != nil {
+				lg.RequestLog(r).WithError(err).Error("Failed to acquire transaction")
+				render.Status(r, http.StatusInternalServerError)
+				render.JSON(w, r, resUsersErr{
+					Message: "Failed transaction",
+				})
+				return
+			}
+			usermodel, err := user.SelectByUsername(tx, username)
+			if err != nil {
+				lg.RequestLog(r).WithError(err).Warn("User not found")
+				render.Status(r, http.StatusNotFound)
+				render.JSON(w, r, resUsersErr{
+					Message: "User not found",
+				})
+				return
+			}
 			// TODO: status logs
-			usermodel := r.Context().Value(ctxKeyUsermodel).(*user.Model)
 			render.Status(r, http.StatusNotFound)
 			render.JSON(w, r, resUsersPublic{
 				Data: *usermodel.GetPublic(),
@@ -105,7 +88,32 @@ func (rr *userroute) Register(r chi.Router) {
 
 		r.Get("/private", func(w http.ResponseWriter, r *http.Request) {
 			// TODO: protect jwt
-			usermodel := r.Context().Value(ctxKeyUsermodel).(*user.Model)
+			username := chi.URLParam(r, "username")
+			if !isValidUsername(username) {
+				render.Status(r, http.StatusBadRequest)
+				render.JSON(w, r, resUsersErr{
+					Message: "Username must be between 1 and 32 characters",
+				})
+				return
+			}
+			tx, err := rr.repo.Transaction()
+			if err != nil {
+				lg.RequestLog(r).WithError(err).Error("Failed to acquire transaction")
+				render.Status(r, http.StatusInternalServerError)
+				render.JSON(w, r, resUsersErr{
+					Message: "Failed transaction",
+				})
+				return
+			}
+			usermodel, err := user.SelectByUsername(tx, username)
+			if err != nil {
+				lg.RequestLog(r).WithError(err).Warn("User not found")
+				render.Status(r, http.StatusNotFound)
+				render.JSON(w, r, resUsersErr{
+					Message: "User not found",
+				})
+				return
+			}
 			render.Status(r, http.StatusNotFound)
 			render.JSON(w, r, resUsersPrivate{
 				Data: *usermodel.GetPrivate(),
@@ -114,57 +122,77 @@ func (rr *userroute) Register(r chi.Router) {
 		})
 	})
 
-	r.Get("/id/:userid", func(c echo.Context) error {
-		userid := c.Param("userid")
-
-		var tx repo.Tx
-		var err error
-		if tx, err = r.repo.Transaction(); err != nil {
-			log.Errorf("transaction: %s", err)
-			return c.JSON(http.StatusInternalServerError, resUsersErr{
+	r.Get("/id/:userid", func(w http.ResponseWriter, r *http.Request) {
+		userid := chi.URLParam(r, "userid")
+		if !isValidUserid(userid) {
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, resUsersErr{
+				Message: "Invalid userid",
+			})
+			return
+		}
+		tx, err := rr.repo.Transaction()
+		if err != nil {
+			lg.RequestLog(r).WithError(err).Error("Failed to acquire transaction")
+			render.Status(r, http.StatusInternalServerError)
+			render.JSON(w, r, resUsersErr{
 				Message: "Failed transaction",
 			})
+			return
 		}
-
 		usermodel, err := user.Select(tx, userid)
 		if err != nil {
-			log.Warnf("Select user by id: %s", err)
-			return c.JSON(http.StatusNotFound, resUsersErr{
-				Message: "Failed to find user",
+			lg.RequestLog(r).WithError(err).Warn("User not found")
+			render.Status(r, http.StatusNotFound)
+			render.JSON(w, r, resUsersErr{
+				Message: "User not found",
 			})
+			return
 		}
-
-		return c.JSON(http.StatusOK, resUsersPublic{
+		render.Status(r, http.StatusNotFound)
+		render.JSON(w, r, resUsersPublic{
 			Data: *usermodel.GetPublic(),
 		})
+		return
 	})
 
-	g.GET("/id/:userid/private", func(c echo.Context) error {
-		userid := c.Param("userid")
-
-		var tx repo.Tx
-		var err error
-		if tx, err = r.repo.Transaction(); err != nil {
-			log.Errorf("transaction: %s", err)
-			return c.JSON(http.StatusInternalServerError, resUsersErr{
+	r.Get("/id/:userid/private", func(w http.ResponseWriter, r *http.Request) {
+		// TODO: get jwt middleware
+		userid := chi.URLParam(r, "userid")
+		if !isValidUserid(userid) {
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, resUsersErr{
+				Message: "Invalid userid",
+			})
+			return
+		}
+		tx, err := rr.repo.Transaction()
+		if err != nil {
+			lg.RequestLog(r).WithError(err).Error("Failed to acquire transaction")
+			render.Status(r, http.StatusInternalServerError)
+			render.JSON(w, r, resUsersErr{
 				Message: "Failed transaction",
 			})
+			return
 		}
-
 		usermodel, err := user.Select(tx, userid)
 		if err != nil {
-			log.Warnf("Select user by id: %s", err)
-			return c.JSON(http.StatusNotFound, resUsersErr{
-				Message: "Failed to find user",
+			lg.RequestLog(r).WithError(err).Warn("User not found")
+			render.Status(r, http.StatusNotFound)
+			render.JSON(w, r, resUsersErr{
+				Message: "User not found",
 			})
+			return
 		}
-
-		return c.JSON(http.StatusOK, resUsersPrivate{
+		render.Status(r, http.StatusNotFound)
+		render.JSON(w, r, resUsersPrivate{
 			Data: *usermodel.GetPrivate(),
 		})
-	}) // TODO: get jwt middleware
+		return
+	})
 
-	g.POST("/:code", func(c echo.Context) error {
-		return c.String(http.StatusOK, fmt.Sprintf("POST /users"))
+	r.Post("/:code", func(w http.ResponseWriter, r *http.Request) {
+		code := chi.URLParam(r, "code")
+		render.PlainText(w, r, fmt.Sprintf("create new user %s", code))
 	})
 }
